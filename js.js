6fc88748aac3ae24ac1ca1b29f42c8cf2b8b531e
Alexander Oboje <obojealexander@gmail.com>
2022-06-30 09:06:54 +0300

    Added new block projects

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Гугл всему голова')
+	alert('Я люблю Дашу')
 })
